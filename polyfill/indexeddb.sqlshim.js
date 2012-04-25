7b50cf2c864e70c8513b10cbe30c7461b4447367
Parashuram <n.parashuram@gmail.com>
2012-04-24 20:57:18 -0700

    Added IndexedDB polyfill using WebSql

--- polyfill/indexeddb.sqlshim.js
+++ polyfill/indexeddb.sqlshim.js
@@ -9,15 +9,20 @@ window.indexedDB = (function(window, undefined){
 	// The sysDB to keep track of version numbers for databases
 	var sysdb = window.openDatabase("__sysdb__", 1, "System Database", DEFAULT_DB_SIZE);
 	sysdb.transaction(function(tx){
-		window.axe = tx.executeSql("SELECT * FROM versions", [], function(t, data){
+		tx.executeSql("SELECT * FROM dbVersions", [], function(t, data){
+			// dbVersions already exists
 		}, function(){
+			// dbVersions does not exist, so creating it
 			sysdb.transaction(function(tx){
-				tx.executeSql("CREATE TABLE versions (name VARCHAR(255), version INT);", [], function(){
+				tx.executeSql("CREATE TABLE IF NOT EXISTS dbVersions (name VARCHAR(255), version INT);", [], function(){
 				}, function(){
 					throw new Error("Could not create table __sysdb__ to save DB versions");
 				});
 			});
 		});
+	}, function(){
+		// sysdb Transaction failed
+		throw new Error("Could not create table __sysdb__ to save DB versions");
 	});
 	
 	/**
@@ -35,21 +40,39 @@ window.indexedDB = (function(window, undefined){
 	}
 	
 	/**
+	 * Throws a new DOM Exception,
+	 * @param {Object} name
+	 * @param {Object} message
+	 * @param {Object} error
+	 */
+	function throwDOMException(name, message, error){
+		console.log(name, message, error);
+		var e = new DOMException.constructor(0, message);
+		e.name = name;
+		e.message = message;
+		e.stack = arguments.callee.caller;
+		throw e;
+	}
+	
+	/**
 	 * The IDBRequest Object that is returns for all async calls
 	 * http://dvcs.w3.org/hg/IndexedDB/raw-file/tip/Overview.html#request-api
 	 */
 	var IDBRequest = function(){
-		this.onsuccess = this.onerror = null;
-		this.result = this.error = null;
-		this.source = this.transaction = null;
+		this.onsuccess = this.onerror = this.result = this.error = this.source = this.transaction = null;
 		this.readyState = "pending";
 	};
-	
+	/**
+	 * The IDBOpen Request called when a database is opened
+	 */
 	var IDBOpenRequest = function(){
 		this.onblocked = this.onupgradeneeded = null;
 	}
 	IDBOpenRequest.prototype = IDBRequest;
 	
+	/**
+	 * Event Object, emulating the browser events
+	 */
 	var Event = function(){
 		this.type = null;
 	};
@@ -88,13 +111,46 @@ window.indexedDB = (function(window, undefined){
 	 * @param {Object} name
 	 * @param {Object} transaction
 	 */
-	var IDBObjectStore = function(name, transaction){
+	var IDBObjectStore = function(name, transaction, ready){
 		this.name = name;
 		this.transaction = transaction;
+		this.__ready = (typeof ready === "undefined") ? true : false;
+	};
+	
+	IDBObjectStore.prototype.__getStoreProps = function(callback){
+		var me = this;
+		this.transaction.db.__db.transaction(function(tx){
+			tx.executeSql("SELECT * FROM __sys__ where name = ?", [me.name], function(tx, data){
+				callback(data.rows.item(0));
+			}, function(){
+				callback();
+			});
+		});
+	};
+	
+	IDBObjectStore.prototype.__getKey = function(props, value, key){
+		if (!props) throwDOMException(0, "Data Error - Could not locate defination for this table", props);
+		if (!props.keyPath && !key) throwDOMException(0, "Data Error - keyPath was defined as null, but a key was provided", props);
+		if (props.keyPath && key) throwDOMException(0, "Data Error - keyPath was defined, but a key was NOT provided", props);
+		if (props.keyPath) {
+			try {
+				key = eval("value" + props.keyPath)
+			} catch (e) {
+			
+			}
+		}
 	};
 	
 	IDBObjectStore.prototype.put = function(value, key){
-		console.log(this);
+		var idbRequest = new IDBRequest();
+		var e = new Event();
+		var me = this;
+		this.__getStoreProps(function(props){
+			me.__checkInsert(props, value, key);
+			me.transaction.db.__db.transaction(function(tx){
+				tx.executeSql("")
+			});
+		});
 	};
 	
 	/**
@@ -103,9 +159,7 @@ window.indexedDB = (function(window, undefined){
 	 * @param {Object} db
 	 */
 	var IDBDatabase = function(db, name, version, storeProperties){
-		this.__db = db;
-		this.version = version;
-		this.__storeProperties = storeProperties;
+		this.__db = db, this.version = version, this.__storeProperties = storeProperties;
 		this.objectStoreNames = [];
 		for (var i = 0; i < storeProperties.rows.length; i++) {
 			this.objectStoreNames.push(storeProperties.rows.item(i).name);
@@ -115,21 +169,50 @@ window.indexedDB = (function(window, undefined){
 	};
 	
 	IDBDatabase.prototype.createObjectStore = function(storeName, createOptions){
-		this.__db.transaction(function(tx){
-			var sql = ["CREATE TABLE", storeName, "(key", (createOptions.authIncrement ? "INT AUTOINCREMENT" : "VARCHAR(255)"), "NOT NULL UNIQUE , value VARCHAR(1000))"].join(" ");
+		createOptions = createOptions || {};
+		createOptions.keyPath = createOptions.keyPath || null;
+		function error(){
+			throwDOMException(0, "Could not create new database", arguments);
+		}
+		var me = this;
+		if (me.transaction && me.transaction.mode !== 2) {
+			throwDOMException(0, "Invalid State error", me.transaction);
+		}
+		
+		var result = new IDBObjectStore(storeName, me.transaction, false);
+		// Should not push here. Pushing here does not gurantee that it was correct
+		me.objectStoreNames.push(storeName);
+		
+		me.__db.transaction(function(tx){
+			//key INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE
+			var sql = ["CREATE TABLE", storeName, "(key", createOptions.autoIncrement ? "INTEGER" : "VARCHAR(255)", "PRIMARY KEY", createOptions.autoIncrement ? "AUTOINCREMENT" : "", " NOT NULL UNIQUE , value VARCHAR(10000))"].join(" ");
+			//console.log(sql);
 			tx.executeSql(sql, [], function(tx, data){
-			}, function(){
-				throw new DOMException.constructor(0, "Could not create new database");
-			});
-			tx.executeSql("INSERT INTO __sys__ VALUES (?,?, ?)", [storeName, createOptions.keyPath, createOptions.autoIncrement ? true : false]);
+				tx.executeSql("INSERT INTO __sys__ VALUES (?,?,?)", [storeName, createOptions.keyPath, createOptions.autoIncrement ? true : false], function(){
+					result.__ready = true;
+				}, error);
+			}, error);
 		});
+		
+		// The IndexedDB Specification needs us to return an Object Store immediatly, but WebSQL does not create and return the store immediatly
+		// Hence, this can technically be unusable, and we hack around it, by setting the ready value to false
+		return result;
 	};
 	
 	IDBDatabase.prototype.deleteObjectStore = function(storeName){
-		this.__db.transaction(function(tx){
-			tx.executeSql("DROP TABLE " + storeName);
-			tx.executeSql("DELETE FROM __sys__ WHERE name = ?", storeName);
-			this.objectStoreNames.splice(this.objectStoreNames.indexOf(storeName), 1);
+		var error = function(){
+			throwDOMException(0, "Could not create new database", arguments);
+		}
+		var me = this;
+		if (me.transaction && me.transaction.mode !== 2) {
+			throwDOMException(0, "Invalid State error", me.transaction);
+		}
+		me.objectStoreNames.splice(me.objectStoreNames.indexOf(storeName), 1);
+		me.__db.transaction(function(tx){
+			tx.executeSql("DROP TABLE " + storeName, [], function(){
+				tx.executeSql("DELETE FROM __sys__ WHERE name = ?", [storeName], function(){
+				}, error);
+			}, error);
 		});
 	};
 	
@@ -143,6 +226,63 @@ window.indexedDB = (function(window, undefined){
 	};
 	
 	return {
+		"deleteDatabase": function(name){
+			var req = new IDBOpenRequest();
+			var e = new Event();
+			var calledDBError = false;
+			function dbError(msg){
+				if (calledDBError) {
+					return;
+				}
+				req.readyState = "done";
+				req.error = "DOMError";
+				e.type = "error";
+				e.message = msg;
+				e.debug = arguments;
+				callback("onerror", req, [e]);
+				calledDBError = true;
+			}
+			
+			sysdb.transaction(function(systx){
+				systx.executeSql("SELECT * FROM dbVersions where name = ?", [name], function(tx, data){
+					if (data.rows.length === 0) {
+						dbError("Database does not exist");
+						return;
+					}
+					var version = data.rows.item(0).version;
+					var db = window.openDatabase(name, 1, name, DEFAULT_DB_SIZE);
+					db.transaction(function(tx){
+						tx.executeSql("SELECT * FROM __sys__", [], function(tx, data){
+							var tables = data.rows;
+							(function deleteTables(i){
+								if (i >= tables.length) {
+									// If all tables are deleted, delete the housekeeping tables
+									tx.executeSql("DROP TABLE __sys__", [], function(){
+										// Finally, delete the record for this DB from sysdb
+										sysdb.transaction(function(systx){
+											systx.executeSql("DELETE FROM dbVersions where name = ? ", [name], function(){
+												req.result = undefined;
+												e.type = "success", e.newVersion = null, e.oldVersion = version;
+												callback("onsuccess", req, [e]);
+											}, dbError);
+										}, dbError);
+									}, dbError);
+								} else {
+									// Delete all tables in this database, maintained in the sys table
+									tx.executeSql("DROP TABLE " + tables.item(i).name, [], function(){
+										deleteTables(i + 1);
+									}, function(){
+										deleteTables(i + 1);
+									});
+								}
+							}(0));
+						});
+					}, dbError);
+				});
+			}, dbError);
+			return req;
+		},
+		
 		/**
 		 * The IndexedDB Method to create a new database and return the DB
 		 * @param {Object} name
@@ -151,53 +291,60 @@ window.indexedDB = (function(window, undefined){
 		"open": function(name, version){
 			var req = new IDBOpenRequest();
 			var e = new Event();
+			var calledDbCreateError = false;
+			function dbCreateError(){
+				if (calledDbCreateError) {
+					return;
+				}
+				req.readyState = "done";
+				req.error = "DOMError";
+				e.type = "error";
+				e.debug = arguments;
+				callback("onerror", req, [e]);
+				calledDbCreateError = true
+			}
 			
 			function openDB(oldVersion){
-				var db = window.openDatabase(name, version, name, DEFAULT_DB_SIZE);
+				var db = window.openDatabase(name, 1, name, DEFAULT_DB_SIZE);
 				req.readyState = "done";
 				if (version <= 0 || oldVersion > version) {
-					throw new DOMException.constructor(0, "An attempt was made to open a database using a lower version than the existing version.");
+					throwDOMException(0, "An attempt was made to open a database using a lower version than the existing version.", version);
 				}
 				db.transaction(function(tx){
-					tx.executeSql("CREATE TABLE IF NOT EXISTS __sys__ (name VARCHAR(255), keyPath VARCHAR(255), autoIncrement boolean)");
-					tx.executeSql("SELECT * FROM __sys__", [], function(tx, data){
-						e.type = "success";
-						req.source = req.result = new IDBDatabase(db, name, version, data);
-						if (oldVersion < version) {
-							e.oldVersion = oldVersion;
-							e.newVersion = version;
-							callback("onupgradeneeded", req, [e]);
-						}
-						callback("onsuccess", req, [e]);
-					}, function(){
-						e.type = "error";
-						callback("onerror", req, [e]);
-					});
-				});
+					tx.executeSql("CREATE TABLE IF NOT EXISTS __sys__ (name VARCHAR(255), keyPath VARCHAR(255), autoInc BOOLEAN)", [], function(){
+						tx.executeSql("SELECT * FROM __sys__ where name = ?", [name], function(tx, data){
+							e.type = "success";
+							req.source = req.result = new IDBDatabase(db, name, version, data);
+							if (oldVersion < version) {
+								sysdb.transaction(function(systx){
+									systx.executeSql("UPDATE dbVersions set version = ? where name = ?", [version, name], function(){
+										e.oldVersion = oldVersion, e.newVersion = version;
+										req.result.transaction([], 2);
+										callback("onupgradeneeded", req, [e]);
+										req.result.transaction = null;
+										callback("onsuccess", req, [e]);
+									}, dbCreateError);
+								}, dbCreateError);
+							} else {
+								callback("onsuccess", req, [e]);
+							}
+						}, dbCreateError);
+					}, dbCreateError);
+				}, dbCreateError);
 			};
 			
 			sysdb.transaction(function(tx){
-				tx.executeSql("SELECT * FROM versions where name = ?;", [name], function(tx, data){
-					// A DB with this name already exists
+				tx.executeSql("SELECT * FROM dbVersions where name = ?", [name], function(tx, data){
 					if (data.rows.length === 0) {
-						tx.executeSql("INSERT INTO versions VALUES (?,?)", [name, version], function(){
-							openDB(0);
-						}, function(){
-							req.readyState = "done";
-							req.error = "DOMError";
-							e.type = "error";
-							callback("onerror", req, [e]);
-						});
+						// Database with this name does not exist
+						tx.executeSql("INSERT INTO dbVersions VALUES (?,?)", [name, version || 1], function(){
+							openDB(1);
+						}, dbCreateError);
 					} else {
 						openDB(data.rows.item(0).version);
 					}
-				}, function(){
-					req.readyState = "done";
-					req.error = "DOMError";
-					e.type = "error";
-					callback("onerror", req, [e]);
-				});
-			});
+				}, dbCreateError);
+			}, dbCreateError);
 			return req;
 		}
 	}
