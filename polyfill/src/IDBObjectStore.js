3663321818285318c2ba63fe8bec403086f857d2
Parashuram <n.parashuram@gmail.com>
2012-04-30 09:44:36 -0700

    Added code to add data to object store using transaction queue.

--- polyfill/src/IDBObjectStore.js
+++ polyfill/src/IDBObjectStore.js
@@ -16,18 +16,16 @@
 	 * Gets (and optionally caches) the properties like keyPath, autoincrement, etc for this objectStore
 	 * @param {Object} callback
 	 */
-	IDBObjectStore.prototype.__getStoreProps = function(callback){
+	IDBObjectStore.prototype.__getStoreProps = function(tx, callback){
 		var me = this;
 		if (me.__storeProps) {
 			modules.util.callback(me.__storeProps);
 		} else {
-			this.transaction.db.__db.transaction(function(tx){
-				tx.executeSql("SELECT * FROM __sys__ where name = ?", [me.name], function(tx, data){
-					me.__storeProps = data.rows.item(0)
-					modules.util.callback(me.__storeProps);
-				}, function(){
-					modules.util.callback();
-				});
+			tx.executeSql("SELECT * FROM __sys__ where name = ?", [me.name], function(tx, data){
+				me.__storeProps = data.rows.item(0)
+				callback(me.__storeProps);
+			}, function(){
+				callback();
 			});
 		}
 	};
@@ -40,36 +38,33 @@
 	 */
 	IDBObjectStore.prototype.__getKey = function(props, value, key){
 		if (!props) modules.util.throwDOMException(0, "Data Error - Could not locate defination for this table", props);
-		if (!props.keyPath && !key) modules.util.throwDOMException(0, "Data Error - keyPath was defined as null, but a key was provided", props);
-		if (props.keyPath && key) modules.util.throwDOMException(0, "Data Error - keyPath was defined, but a key was NOT provided", props);
+		if (props.keyPath && key) modules.util.throwDOMException(0, "Data Error - The object store uses in-line keys and the key parameter was provided", props);
+		if (!props.keyPath && props.autoInc === "false" && !key) modules.util.throwDOMException(0, "Data Error - The object store uses out-of-line keys and has no key generator and the key parameter was not provided. ", props);
 		if (props.keyPath) {
 			try {
-				key = eval("value" + props.keyPath)
+				key = eval("value" + props.keyPath);
 			} catch (e) {
 			
 			}
 		}
+		return key;
 	};
-	
-	/**
-	 * Checks if an insert is allowed in this object
-	 * @param {Object} props
-	 * @param {Object} value
-	 * @param {Object} key
-	 */
-	IDBObjectStore.prototype.__getKey = function(props, value, key){
-		//TODO Check insert constraints
-	}
 	
 	IDBObjectStore.prototype.add = function(value, key){
 		var me = this;
-		return me.transaction.__addToTransactionQueue(function(tx, args){
-			this.__getStoreProps(function(props){
-				me.__checkInsert(props, value, key);
-				tx.executeSql(function(){
-				
-				}, function(){
-				
+		return me.transaction.__addToTransactionQueue(function(tx, args, success, error){
+			me.__getStoreProps(tx, function(props){
+				var primaryKey = me.__getKey(props, value, key);
+				var sqlValues = [JSON.stringify(value)];
+				var sql = ["INSERT INTO", me.name];
+				sql.push(!primaryKey ? "(value)" : "(value, key)");
+				sql.push("VALUES (?");
+				primaryKey && (sql.push(",?"), sqlValues.push(primaryKey));
+				sql.push(")");
+				tx.executeSql(sql.join(" "), sqlValues, function(tx, data){
+					success(primaryKey);
+				}, function(tx, err){
+					error(err);
 				});
 			});
 		});
