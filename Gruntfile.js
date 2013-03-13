163d1cc790513d23b6cd1cd145b72fa43b95bf95
Parashuram <code@r.nparashuram.com>
2013-03-12 21:18:56 -0700

    Changed the look of the home page

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          base: ".",
+          port: 8080
+        }
+      }
+    },
+    watch: {}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+  // default task (no deploy)
+  grunt.registerTask('default', ['connect', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# IndexedDB 
+
+[IndexedDB](http://www.w3.org/TR/IndexedDB/) is a client side sorage specification.
